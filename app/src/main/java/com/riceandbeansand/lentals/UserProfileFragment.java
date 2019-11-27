3df71e99ad82a1674771193c345307f909a1aba3
Manan Aggarwal <mananaggarwal99@gmail.com>
2019-11-27 03:24:56 -0500

    created user profile and listings

--- app/src/main/java/com/riceandbeansand/lentals/UserProfileFragment.java
+++ app/src/main/java/com/riceandbeansand/lentals/UserProfileFragment.java
@@ -1,6 +1,41 @@
 package com.riceandbeansand.lentals;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
-public class userProfileFragment extends Fragment {
+public class UserProfileFragment extends Fragment {
+    private String name;
+    private String userId;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        View view = inflater.inflate(R.layout.user_profile, container, false);
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setTitle("Item Profile");
+
+        Bundle bundle = this.getArguments();
+        if (bundle != null) {
+            name = bundle.getString("name", "UNKNOWN");
+            userId = bundle.getString("userId");
+        }
+
+        ((TextView) view.findViewById(R.id.userNameTitle)).setText(name);
+
+        Bundle args = new Bundle();
+        args.putString("queryType", "userItems");
+        args.putString("userId", userId);
+        Fragment userListings = new ListingsFragment();
+        userListings.setArguments(args);
+        getActivity().getSupportFragmentManager().beginTransaction()
+                .replace(R.id.list_container, userListings).commit();
+
+        return view;
+    }
 }
