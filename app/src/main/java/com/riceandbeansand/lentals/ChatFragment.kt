76ad59adc2e61bde93d00f7f169f48779ec4951b
cameronfr <catechboy@hotmail.com>
2019-12-02 16:27:47 -0500

    fix viewholder issue and improve chat scrolling
    Signed-off-by: cameronfr <catechboy@hotmail.com>

--- app/src/main/java/com/riceandbeansand/lentals/ChatFragment.kt
+++ app/src/main/java/com/riceandbeansand/lentals/ChatFragment.kt
@@ -18,6 +18,9 @@ import com.firebase.ui.firestore.FirestoreRecyclerAdapter
 import com.firebase.ui.firestore.FirestoreRecyclerOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
+import org.w3c.dom.Text
+
 
 class ChatFragment: Fragment() {
 
@@ -33,22 +36,10 @@ class ChatFragment: Fragment() {
         (activity as AppCompatActivity).supportActionBar!!.title = arguments?.getString("name", "UNKNOWN")
 
         val view = inflater.inflate(R.layout.chat, container, false)
-        view.findViewById<Button>(R.id.sendButton).setOnClickListener(View.OnClickListener {
-            val messageTextView = view.findViewById<EditText>(R.id.messageText)
-            val message = messageTextView.text.toString()
-            Log.d("app", "message is " + message)
-            val currentTime = System.currentTimeMillis()
-
-            val docData = hashMapOf(
-                    "message" to message,
-                    "timestamp" to currentTime,
-                    "senderID" to currentUserID
-            )
-            db.collection("chats").document(chatID).collection("messages")
-                    .document().set(docData)
-                    .addOnSuccessListener { messageTextView.setText("")}
-                    .addOnFailureListener { e -> Log.w("App", "Error writing chat msg", e)  }
-        })
+        val recyclerView = view.findViewById<RecyclerView>(R.id.chat_area)
+        val layoutManager = LinearLayoutManager(context)
+        layoutManager.stackFromEnd = true //doesn't seem to be working on keyboard popup
+        recyclerView.layoutManager = layoutManager
 
         val options = FirestoreRecyclerOptions.Builder<ChatItemSchema>()
                 .setQuery(query, ChatItemSchema::class.java)
@@ -56,6 +47,11 @@ class ChatFragment: Fragment() {
                 .build()
 
         val adapter = object : FirestoreRecyclerAdapter<ChatItemSchema, ChatFragment.ViewHolder>(options) {
+            override fun onDataChanged() {
+                super.onDataChanged()
+                recyclerView.scrollToPosition(this.getItemCount() - 1)
+            }
+
             override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChatFragment.ViewHolder {
                 val view = LayoutInflater.from(parent.context)
                         .inflate(R.layout.chat_bubble, parent, false)
@@ -64,19 +60,46 @@ class ChatFragment: Fragment() {
                 return ViewHolder(view)
             }
 
+            // Views are recycled, so a view that was once a sent message might be rebound as a recieved message => must reset color
             override fun onBindViewHolder(holder: ChatFragment.ViewHolder, position: Int, model: ChatItemSchema) {
                 holder.view.findViewById<TextView>(R.id.messageBubbleText).text = model.message
+                var alignment = 0.0f
+                var background = R.drawable.incoming_chat_rounded_rectangle
+                var textColor = R.color.colorAccent
                 if (model.senderID == currentUserID) {
-                    (holder.view.findViewById<View>(R.id.dummyView).layoutParams as LinearLayout.LayoutParams).weight = 1.0f
-                    holder.view.findViewById<TextView>(R.id.messageBubbleText).setBackgroundResource(R.drawable.outgoing_chat_rounded_rectangle)
-                    holder.view.findViewById<TextView>(R.id.messageBubbleText).setTextColor(Color.WHITE)
+                    alignment = 1.0f
+                    background = R.drawable.outgoing_chat_rounded_rectangle
+                    textColor = Color.WHITE
                 }
+                (holder.view.findViewById<View>(R.id.dummyView).layoutParams as LinearLayout.LayoutParams).weight = alignment
+                holder.view.findViewById<TextView>(R.id.messageBubbleText).setBackgroundResource(background)
+                holder.view.findViewById<TextView>(R.id.messageBubbleText).setTextColor(textColor)
             }
         }
 
-        val recyclerView = view.findViewById<RecyclerView>(R.id.chat_area)
-        recyclerView.layoutManager = LinearLayoutManager(context)
         recyclerView.adapter = adapter
+
+        view.findViewById<Button>(R.id.sendButton).setOnClickListener(View.OnClickListener {
+            val messageTextView = view.findViewById<EditText>(R.id.messageText)
+            val message = messageTextView.text.toString()
+            messageTextView.setText("")
+            val currentTime = System.currentTimeMillis()
+
+            val docData = hashMapOf(
+                    "message" to message,
+                    "timestamp" to currentTime,
+                    "senderID" to currentUserID
+            )
+
+            db.collection("chats").document(chatID).collection("messages")
+                    .document().set(docData)
+                    .addOnSuccessListener {
+                    }
+                    .addOnFailureListener { e ->
+                        Log.w("App", "Error writing chat msg", e)
+                        messageTextView.setText(message)
+                    }
+        })
 
         return view
     }
