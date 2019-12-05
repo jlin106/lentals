c8d9cc046ad468ebbe429690f6e0b6e4f5243e96
Reem Larabi <rlarabi3279@gmail.com>
2019-12-05 01:52:17 -0500

    images, distances, names to item markers
    Signed-off-by: Reem Larabi <rlarabi3279@gmail.com>

--- app/src/main/java/com/riceandbeansand/lentals/MapFragment.kt
+++ app/src/main/java/com/riceandbeansand/lentals/MapFragment.kt
@@ -3,7 +3,12 @@ package com.riceandbeansand.lentals
 import android.Manifest
 import android.content.ContentValues.TAG
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.graphics.Color
 import android.location.Location
+import android.media.ThumbnailUtils
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -20,6 +25,7 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.gms.tasks.OnSuccessListener
@@ -35,6 +41,8 @@ import kotlin.coroutines.resumeWithException
 import kotlin.coroutines.suspendCoroutine
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import java.math.BigDecimal
+import java.math.RoundingMode
 
 class MapFragment : Fragment(), OnMapReadyCallback {
 
@@ -43,7 +51,7 @@ class MapFragment : Fragment(), OnMapReadyCallback {
     private val dbsize = 0
 
     //should i change double to LatLng
-    var coll_items: HashMap<String, LatLng> = hashMapOf();
+    var coll_items: HashMap<String, LatLng> = hashMapOf()
     //each item in array list is a mapping of an item name to an array containing the LAT_LONG coords
 
 
@@ -87,35 +95,58 @@ class MapFragment : Fragment(), OnMapReadyCallback {
         }
     }
 
+    fun getDistanceBetween(latLng1: LatLng, latLng2: LatLng): Double {
+        val result = FloatArray(1)
+        Location.distanceBetween(latLng1!!.latitude, latLng1!!.longitude,
+                latLng2!!.latitude, latLng2!!.longitude, result)
+        val decimal = BigDecimal(result[0].toDouble() / 1609.344).setScale(2, RoundingMode.HALF_EVEN)
+        return decimal.toDouble()
+    }
+
+    fun addBorder(bmp: Bitmap, borderSize: Int): Bitmap {
+        val bmpWithBorder = Bitmap.createBitmap(bmp.width + borderSize * 2, bmp.height + borderSize * 2, bmp.getConfig());
+        val canvas = Canvas(bmpWithBorder);
+        canvas.drawColor(Color.BLACK);
+        canvas.drawBitmap(bmp, borderSize.toFloat(), borderSize.toFloat(), null);
+        return bmpWithBorder;
+    }
+
     fun initializeMap() {
         GlobalScope.launch(Dispatchers.Main) {
             val currentLocation = fetchLocation()!!
-            val latLng = LatLng(currentLocation.getLatitude(), currentLocation.getLongitude())
+            val currentLatLng = LatLng(currentLocation.getLatitude(), currentLocation.getLongitude())
             val db = FirebaseFirestore.getInstance()
 
             db.collection("items")
                     .get()
                     .addOnSuccessListener { result ->
-                        var stringyboi = 0
+                        var numCloseItems = 0
                         for (document in result) {
                             Log.d(TAG, "${document.id} => ${document.data}")
-                            stringyboi += 1
-                            //definitely did this wrong
+                            numCloseItems += 1
                             val latlong = document.get("lat_long") as List<Double>
-                            val latlongObj = LatLng(latlong[0], latlong[1])
-                            coll_items.put(document.getString("name").toString(), latlongObj)
+                            val itemPosition = LatLng(latlong[0], latlong[1])
+                            var dist = getDistanceBetween(currentLatLng, itemPosition)
+                            getImageFileFromGSUrlWithCache(document.getString("imagePath")!!, activity!!.cacheDir){file ->
+                                var imageBitmap = BitmapFactory.decodeFile(file.absolutePath) //should scale this down
+                                imageBitmap = ThumbnailUtils.extractThumbnail(imageBitmap, 100, 100)
+                                imageBitmap = addBorder(imageBitmap, 4);
+                                val itemImageIcon = BitmapDescriptorFactory.fromBitmap(imageBitmap);
+                                val markerOptions = MarkerOptions().position(itemPosition)
+                                        .title(document.getString("name"))
+                                        .snippet(dist.toString() +  " mi away")
+                                        .icon(itemImageIcon)
+                                //googleMap!!.animateCamera(CameraUpdateFactory.newLatLng(latLng))
+                                //googleMap!!.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10f))
+                                googleMap!!.addMarker(markerOptions)
+                            }
+
                         }
-                        for (item in coll_items) {
-                            //did i call this correctly
-                            val markerOptions = MarkerOptions().position(item.value).title(item.key)
-                            googleMap!!.animateCamera(CameraUpdateFactory.newLatLng(latLng))
-                            googleMap!!.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 5f))
-                            googleMap!!.addMarker(markerOptions)
-                        }
-                        val markerOptions = MarkerOptions().position(latLng).title( "Available things to borrow: " + stringyboi)
-                        googleMap!!.animateCamera(CameraUpdateFactory.newLatLng(latLng))
-                        googleMap!!.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 5f))
-                        googleMap!!.addMarker(markerOptions)
+                        //val markerOptions = MarkerOptions().position(currentLatLng).title( "Available things to borrow: " + numCloseItems)
+                       // googleMap!!.animateCamera(CameraUpdateFactory.newLatLng(currentLatLng))
+                        val soup = LatLng(39.3292205,-76.6189553)
+                        googleMap!!.animateCamera(CameraUpdateFactory.newLatLngZoom(soup, 16f))
+                        //googleMap!!.addMarker(markerOptions)
                     }
                     .addOnFailureListener { exception ->
                         Log.d(TAG, "Error getting documents: ", exception)
